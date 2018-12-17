894883fc162dbc34b7816185933a0baffc19e272
v.shadrin <v.shadrin@qsoft.ru>
2018-12-17 04:28:21 +0300

    Добавил другой вариант скрипта - с последовательным выводом информации, однако скорость выполнения и затрачиваемые ресурсы памяти одинаковы

--- parser/parser2.php
+++ parser/parser2.php
@@ -23,9 +23,11 @@ fclose($file);
 // количество просмотров
 $viewsPattern = '~([0-9]{1,3}[\.]){3}[0-9]{1,3}~'; 
 $viewsCount = count(parse($viewsPattern, $content));
+echo '{' . "\n\t" . 'views: ' . $viewsCount . ',' . "\n\t";
 // количество уникальных url
 $urlsPattern = '~"(http|https|ftp)://([A-Z0-9][A-Z0-9_-]*(?:.[A-Z0-9][A-Z0-9_-]*)+):?(d+)?/?"~i';
 $urlsCount = count(array_unique(parse($urlsPattern, $content)));
+echo 'urls: ' . $urlsCount . ',' . "\n\t";
 // объем трафика
 $trafficPattern = '~ [0-9]+ "~';
 $sumTraffic = 0;
@@ -33,41 +35,35 @@ $traffic = parse($trafficPattern, $content);
 foreach ($traffic as $value) {
 	$sumTraffic += $value; 
 }
+echo 'traffic: ' . $sumTraffic . ',' . "\n\t";
 // количество строк в файле
 $linesPattern = '~$~m';
 $linesCount = count(parse($linesPattern, $content));
+echo 'lines: ' . $linesCount . ',' . "\n\t";
+
+//поисковые роботы
+echo 'crawlers: {' . "\n";
+$gooleBot = count(parse('~Google~', $content));
+echo "\t\t" . 'Google: ' . $gooleBot . ',' . "\n";
+$bingBot = count(parse('~bingbot~', $content));
+echo "\t\t" . 'Bing: ' . $bingBot . ',' . "\n";
+$ramblerBot = count(parse('~StackRambler~', $content));
+echo "\t\t" . 'Rambler: ' . $ramblerBot . ',' . "\n";
+$yahooBot = count(parse('~Yahoo! Slurp~', $content));
+echo "\t\t" . 'Yahoo: ' . $yahooBot . ',' . "\n";
+$baiduBot = count(parse('~Baiduspider~', $content));
+echo "\t\t" . 'Baidu: ' . $baiduBot . ',' . "\n";
+$yandexBot = count(parse('~Yandex~', $content));
+echo "\t\t" . 'Yandex: ' . $yandexBot . ',' . "\n";
+$mailruBot = count(parse('~Mail.Ru_Bot~', $content));
+echo "\t\t" . 'Mail.ru: ' . $mailruBot . "\n\t" . '},' . "\n\t";
+
 //коды состояний
+echo 'statusCodes: {' . "\n\t\t";
 $statusCodesPattern = '~" [0-9]{3} ~';
 $statusCodes = '';
 $arrayOfCodes = array_count_values(parse($statusCodesPattern, $content));
 foreach ($arrayOfCodes as $code => $count) {
 	$statusCodes .= substr($code, 2, -1) . ': ' . $count . "\n\t\t";
-}
-//поисковые роботы
-$gooleBot = count(parse('~Google~', $content));
-$bingBot = count(parse('~bingbot~', $content));
-$ramblerBot = count(parse('~StackRambler~', $content));
-$yahooBot = count(parse('~Yahoo! Slurp~', $content));
-$baiduBot = count(parse('~Baiduspider~', $content));
-$yandexBot = count(parse('~Yandex~', $content));
-$mailruBot = count(parse('~Mail.Ru_Bot~', $content));
-
-echo 
-'{
-	views: ' . $viewsCount . ',
-	urls: ' . $urlsCount . ',
-	traffic: ' . $sumTraffic . ',
-	lines: ' . $linesCount . ',
-	crawlers: {
-		Google: ' . $gooleBot . ',
-		Bing: ' . $bingBot . ',
-		Rambler: ' . $ramblerBot . ',
-		Yahoo: ' . $yahooBot . ',
-		Baidu: ' . $baiduBot . ',
-		Yandex: ' . $yandexBot . ',
-		Mail.ru: ' . $mailruBot . '
-	},
-	statusCodes: {
-		' . substr($statusCodes, 0, -2) . 
-'	}
-}';
+}		
+echo substr($statusCodes, 0, -2) . "\t" . '}' . "\n" . '}';
